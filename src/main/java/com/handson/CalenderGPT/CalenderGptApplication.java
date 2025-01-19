f1372aa99d7ed12ca1270794236f073f608bd6ce
root <barschtalman@gmail.com>
2025-01-19 15:17:52 +0200

    with swagger

---
+++ src/main/java/com/handson/CalenderGPT/CalenderGptApplication.java
@@ -0,0 +1,13 @@
+package com.handson.CalenderGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenderGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handson/CalenderGPT/CalenderGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handson.CalenderGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
